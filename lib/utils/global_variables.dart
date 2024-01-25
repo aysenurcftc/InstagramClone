c5f96eda336df05f2abe821be6c32122724b914e
aysenur <aysenurfortuna@gmail.com>
2024-01-25 22:58:56 +0300

    profile screen added

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instacloneapp/screens/add_post_screen.dart';
 import 'package:instacloneapp/screens/feed_screen.dart';
+import 'package:instacloneapp/screens/profile_screen.dart';
+import 'package:instacloneapp/screens/search_screen.dart';
 
 
 const webScreenSize = 600;
@@ -8,9 +11,11 @@ const webScreenSize = 600;
 List<Widget> homeScreenItems = [
 
   FeedScreen(),
-  Text("hello"),
+  SearchScreen(),
   AddPostScreen(),
   Text("hello"),
-  Text("hello"),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 
 ];
